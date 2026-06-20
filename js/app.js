37f09a9cac5d82ca0fb5e49f09957adcdfcd9f59
ArgoVoid <argovoid.studio@gmail.com>
2026-06-20 22:51:10 +0330

    feat: clear search input after city search

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const screens = document.querySelectorAll("main > section"); // All screen secti
 
 const navButtons = document.querySelectorAll("nav button"); // Navigation buttons
 
-const errorName = document.getElementById('errorCity')
+const errorName = document.getElementById("errorCity");
 
 const defaultScreen = "empty-screen"; // Screen shown when the page loads
 
@@ -39,31 +39,35 @@ navButtons.forEach((button) => {
 // Display the default screen when the page loads
 showScreen(defaultScreen);
 
-const searchInput = document.getElementById("searchInput");
+const searchInputs = document.querySelectorAll(".search-Input");
 
-searchInput.addEventListener("keydown", async (event) => {
-  if (event.key === "Enter") {
-    const cityName = searchInput.value.trim();
-    if (!cityName) return;
-    showScreen("loading-screen");
-    try {
-      const data = await fetchWeather(cityName);
-      updateUI(data);
+searchInputs.forEach((input) => {
+  input.addEventListener("keydown", async (event) => {
+    له {
+      const cityName = input.value.trim();
+      if (!cityName) return;
+      showScreen("loading-screen");
+      try {
+        const data = await fetchWeather(cityName);
+        updateUI(data);
 
-      const condition = data.weather[0].main;
-      if (
-        condition === "Rain" ||
-        condition === "Drizzle" ||
-        condition === "Thunderstorm"
-      ) {
-        showScreen("rainy-screen");
-      } else {
-        showScreen("clear-screen");
+        const condition = data.weather[0].main;
+        if (
+          condition === "Rain" ||
+          condition === "Drizzle" ||
+          condition === "Thunderstorm"
+        ) {
+          showScreen("rainy-screen");
+        } else {
+          showScreen("clear-screen");
+        }
+        errorName.textContent = "";
+      } catch (error) {
+        errorName.textContent = `"${cityName}" couldn't be found`;
+        showScreen("error-screen");
       }
-    errorName.textContent= ""
-    } catch (error) {
-      errorName.textContent = `"${cityName}" couldn't be found`
-      showScreen("error-screen");
+    input.value = ''
+
     }
-  }
+  });
 });
--- js/ui.js
+++ js/ui.js
@@ -1,19 +1,39 @@
 const temperatureEl = document.getElementById("temperature");
+const temperature_RainyEl = document.getElementById("temperature-rainy");
 const cityNameEl = document.getElementById("cityName");
+const cityName_RainyEl = document.getElementById("cityName-rainy");
 const countryEl = document.getElementById("country");
+const country_RainyEl = document.getElementById("country-rainy");
 const dateTimeEl = document.getElementById("dateTime");
+const dateTime_RainyEl = document.getElementById("dateTime-rainy");
 const descriptionEl = document.getElementById("description");
+const description_RainyEl = document.getElementById("description-rainy");
 const feelsLikeEl = document.getElementById("feelsLike");
+const feelsLike_RainyEl = document.getElementById("feelsLike-rainy");
 const humidityEl = document.getElementById("humidity");
+const humidity_RainyEl = document.getElementById("humidity-rainy");
 const windEl = document.getElementById("wind");
+const wind_RainyEl = document.getElementById("wind-rainy");
 const visibilityEl = document.getElementById("visibility");
+const visibility_RainyEl = document.getElementById("visibility-rainy");
 const uvIndexEl = document.getElementById("uvIndex");
+const uvIndex_RainyEl = document.getElementById("uvIndex-rainy");
 
 function updateUI(data) {
   temperatureEl.textContent = `${data.main.temp}°`;
+  temperature_RainyEl.textContent = `${data.main.temp}°`;
   cityNameEl.textContent = `${data.name}`;
+  cityName_RainyEl.textContent = `${data.name}`;
   countryEl.textContent = `${data.sys.country}`;
+  country_RainyEl.textContent = `${data.sys.country}`;
   dateTimeEl.textContent = new Date().toLocaleString("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  dateTime_RainyEl.textContent = new Date().toLocaleString("en-US", {
     weekday: "long",
     month: "long",
     day: "numeric",
@@ -22,23 +42,31 @@ function updateUI(data) {
   });
 
   descriptionEl.textContent = `${data.weather[0].description}`;
+  description_RainyEl.textContent = `${data.weather[0].description}`;
   feelsLikeEl.textContent = `Feels Like ${data.main.feels_like}°`;
+  feelsLike_RainyEl.textContent = `Feels Like ${data.main.feels_like}°`;
   humidityEl.textContent = `${data.main.humidity}%`;
-  windEl.textContent = `${data.wind.speed} KM/HW`;
+  humidity_RainyEl.textContent = `${data.main.humidity}%`;
+  windEl.textContent = `${data.wind.speed} KM/H`;
+  wind_RainyEl.textContent = `${data.wind.speed} KM/H`;
   visibilityEl.textContent = `${data.visibility} KM`;
+  visibility_RainyEl.textContent = `${data.visibility} KM`;
   uvIndexEl.textContent = `N/A`;
+  uvIndex_RainyEl.textContent = `N/A`;
   updateBackground(data.weather[0].main);
 }
-const condition = data.weather[0].main;
 function updateBackground(condition) {
   document.body.classList = "";
 
   if (condition === "Clear") {
     document.body.classList.add("weather-clear");
-  } else if (condition === "Rain"  || condition === "Drizzle" || condition === "Thunderstorm") {
+  } else if (
+    condition === "Rain" ||
+    condition === "Drizzle" ||
+    condition === "Thunderstorm"
+  ) {
     document.body.classList.add("weather-rainy");
   } else {
     document.body.classList.add("weather-cloudy");
   }
 }
-
