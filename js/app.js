af87eca54ce3c6be0338958236a4a26557a150ea
ArgoVoid <argovoid.studio@gmail.com>
2026-06-23 22:53:25 +0330

    refactor: merge clear and raniy screens

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 const screens = document.querySelectorAll("main > section"); // All screen sections
 
-const navButtons = document.querySelectorAll("nav button"); // Navigation buttons
-
 const errorName = document.getElementById("errorCity");
 
 const defaultScreen = "empty-screen"; // Screen shown when the page loads
@@ -10,31 +8,9 @@ function showScreen(screenName) {
   // Hide all screens
   screens.forEach((screen) => screen.classList.add("hidden"));
 
-  // Remove active state from all buttons
-  navButtons.forEach((btn) => btn.classList.remove("active"));
-
   // Show the selected screen
   document.querySelector("." + screenName).classList.remove("hidden");
-
-  // Highlight the button of the current screen
-  document
-    .querySelector(`[data-screen="${screenName}"]`)
-    .classList.add("active");
 }
-
-navButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    // Get the target screen from the clicked button
-    const screenName = button.dataset.screen;
-
-    if (screenName === "empty-screen") {
-      document.body.className = "";
-    }
-
-    // Switch to the selected screen
-    showScreen(screenName);
-  });
-});
 
 // Display the default screen when the page loads
 showScreen(defaultScreen);
@@ -43,33 +19,38 @@ const searchInputs = document.querySelectorAll(".search-Input");
 
 searchInputs.forEach((input) => {
   input.addEventListener("keydown", async (event) => {
+    
     if (event.key === "Enter") {
+
       const cityName = input.value.trim();
+
       if (!cityName) return;
+
       showScreen("loading-screen");
+
       try {
         const data = await fetchWeather(cityName);
+
         updateUI(data);
+
         const forecastData = await fetchForecast(cityName);
+
         updateForecast(forecastData);
 
         input.value = "";
 
         const condition = data.weather[0].main;
-        if (
-          condition === "Rain" ||
-          condition === "Drizzle" ||
-          condition === "Thunderstorm"
-        ) {
-          showScreen("rainy-screen");
-        } else {
-          showScreen("clear-screen");
-        }
+
+        showScreen("weather-screen");
+
         errorName.textContent = "";
+
       } catch (error) {
         errorName.textContent = `"${cityName}" couldn't be found`;
+
         showScreen("error-screen");
       }
+
       input.value = "";
     }
   });
--- js/ui.js
+++ js/ui.js
@@ -1,42 +1,22 @@
 const temperatureEl = document.getElementById("temperature");
-const temperature_RainyEl = document.getElementById("temperature-rainy");
 const cityNameEl = document.getElementById("cityName");
-const cityName_RainyEl = document.getElementById("cityName-rainy");
 const countryEl = document.getElementById("country");
-const country_RainyEl = document.getElementById("country-rainy");
 const dateTimeEl = document.getElementById("dateTime");
-const dateTime_RainyEl = document.getElementById("dateTime-rainy");
 const descriptionEl = document.getElementById("description");
-const description_RainyEl = document.getElementById("description-rainy");
 const feelsLikeEl = document.getElementById("feelsLike");
-const feelsLike_RainyEl = document.getElementById("feelsLike-rainy");
 const humidityEl = document.getElementById("humidity");
-const humidity_RainyEl = document.getElementById("humidity-rainy");
 const windEl = document.getElementById("wind");
-const wind_RainyEl = document.getElementById("wind-rainy");
+
 const visibilityEl = document.getElementById("visibility");
-const visibility_RainyEl = document.getElementById("visibility-rainy");
 const uvIndexEl = document.getElementById("uvIndex");
-const uvIndex_RainyEl = document.getElementById("uvIndex-rainy");
 
 const weatherIconEl = document.getElementById("weatherIcon");
-const weatherIcon_RainyEl = document.getElementById("weatherIcon-rainy");
 
 function updateUI(data) {
   temperatureEl.textContent = `${data.main.temp}°`;
-  temperature_RainyEl.textContent = `${data.main.temp}°`;
   cityNameEl.textContent = `${data.name} ,`;
-  cityName_RainyEl.textContent = `${data.name} ,`;
   countryEl.textContent = `${data.sys.country}`;
-  country_RainyEl.textContent = `${data.sys.country}`;
   dateTimeEl.textContent = new Date().toLocaleString("en-US", {
-    weekday: "long",
-    month: "long",
-    day: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-  dateTime_RainyEl.textContent = new Date().toLocaleString("en-US", {
     weekday: "long",
     month: "long",
     day: "numeric",
@@ -45,38 +25,26 @@ function updateUI(data) {
   });
 
   descriptionEl.textContent = `${data.weather[0].description}`;
-  description_RainyEl.textContent = `${data.weather[0].description}`;
   feelsLikeEl.textContent = `Feels Like ${data.main.feels_like}°`;
-  feelsLike_RainyEl.textContent = `Feels Like ${data.main.feels_like}°`;
   humidityEl.textContent = `${data.main.humidity}%`;
-  humidity_RainyEl.textContent = `${data.main.humidity}%`;
   windEl.textContent = `${data.wind.speed} KM/H`;
-  wind_RainyEl.textContent = `${data.wind.speed} KM/H`;
   visibilityEl.textContent = `${data.visibility} KM`;
-  visibility_RainyEl.textContent = `${data.visibility} KM`;
   uvIndexEl.textContent = `N/A`;
-  uvIndex_RainyEl.textContent = `N/A`;
 
   const iconCode = data.weather[0].icon;
   const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
   weatherIconEl.src = iconUrl;
-  weatherIcon_RainyEl.src = iconUrl;
 
-  updateBackground(data.weather[0].main);
+  updateBackground(data.weather[0].main, data.weather[0].icon);
 }
 
 function updateForecast(forecasts) {
   const container = document.getElementById("forecastCards");
-  const container_rainy = document.getElementById("forecastCards-rainy");
 
-  // اول محتوای قبلی را پاک کن
   container.innerHTML = "";
-  container_rainy.innerHTML = "";
 
-  // روی هر روز بچرخ و کارت بساز
   forecasts.forEach((item) => {
-    // نام روز را از dt_txt بگیر
     const day = new Date(item.dt_txt).toLocaleDateString("en-US", {
       weekday: "short",
     });
@@ -84,7 +52,6 @@ function updateForecast(forecasts) {
     const maxTemp = Math.round(item.main.temp_max);
     const minTemp = Math.round(item.main.temp_min);
 
-    // کارت بساز
     const cardHTML = `
     <div class="card">
       <h4>${day}</h4>
@@ -94,12 +61,18 @@ function updateForecast(forecasts) {
     </div>
   `;
     container.innerHTML += cardHTML;
-    container_rainy.innerHTML += cardHTML;
   });
 }
 
-function updateBackground(condition) {
+function updateBackground(condition, icon) {
   document.body.classList = "";
+
+  const isNight = icon.endsWith("n");
+
+  if (isNight) {
+    document.body.classList.add("weather-night");
+    return;
+  }
 
   if (condition === "Clear") {
     document.body.classList.add("weather-clear");
