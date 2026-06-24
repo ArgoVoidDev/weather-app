be921b409aa7ef8d9d2dc44582c99425ad7d52a4
ArgoVoid <argovoid.studio@gmail.com>
2026-06-24 09:07:50 +0330

    feat: add suggestion buttons and refactor search into searchCity function

--- js/app.js
+++ js/app.js
@@ -2,17 +2,16 @@ const screens = document.querySelectorAll("main > section");
 
 const errorName = document.getElementById("errorCity");
 
-const defaultScreen = "empty-screen"; 
+const defaultScreen = "empty-screen";
 
 const logos = document.querySelectorAll(".skycast-logo");
 
 function showScreen(screenName) {
-
   screens.forEach((screen) => screen.classList.add("hidden"));
 
   const activeScreen = document.querySelector("." + screenName);
 
-  activeScreen.classList.remove('hidden');
+  activeScreen.classList.remove("hidden");
 
   activeScreen.classList.remove("fade-in");
 
@@ -24,47 +23,46 @@ function showScreen(screenName) {
 // Display the default screen when the page loads
 showScreen(defaultScreen);
 
+
+async function searchCity(cityName) {
+  if (!cityName) return;
+
+  showScreen("loading-screen");
+
+  try {
+    const data = await fetchWeather(cityName);
+    updateUI(data);
+
+    const forecastData = await fetchForecast(cityName);
+    updateForecast(forecastData);
+
+    showScreen("weather-screen");
+    errorName.textContent = "";
+
+  } catch (error) {
+    errorName.textContent = `"${cityName}" couldn't be found`;
+    showScreen("error-screen");
+  }
+}
+
 const searchInputs = document.querySelectorAll(".search-Input");
 
 searchInputs.forEach((input) => {
   input.addEventListener("keydown", async (event) => {
     if (event.key === "Enter") {
       const cityName = input.value.trim();
-
-      if (!cityName) return;
-
-      showScreen("loading-screen");
-
-      try {
-        const data = await fetchWeather(cityName);
-
-        updateUI(data);
-
-        const forecastData = await fetchForecast(cityName);
-
-        updateForecast(forecastData);
-
-        input.value = "";
-
-        const condition = data.weather[0].main;
-
-        showScreen("weather-screen");
-
-        errorName.textContent = "";
-      } catch (error) {
-        errorName.textContent = `"${cityName}" couldn't be found`;
-
-        showScreen("error-screen");
-      }
-
+      await searchCity(cityName);
       input.value = "";
     }
   });
 });
 
-logos.forEach((logo) => {
-  logo.addEventListener("click", () => {
-    showScreen("empty-screen");
-    document.body.className = "";
+
+const suggestionButtons = document.querySelectorAll(".btn-box .btn");
+
+suggestionButtons.forEach((button) => {
+  button.addEventListener("click", async () => {
+    const cityName = button.textContent;
+    await searchCity(cityName);
   });
 });
